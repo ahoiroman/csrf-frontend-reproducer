a56655cb0447e2df5e0338db6f859685b36c19fa
rlossin <roman.lossin@icloud.com>
2023-10-27 11:59:23 +0200

    Added link to backend repo

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>XSRF-Token-Reproducer</h1>
   <p>Hi there, thank you for using this reproducer. In order to get this up and running, you need a Laravel-instance.
-    You may find this here:</p>
+    You may find this here: <a href="https://github.com/ahoiroman/csrf-backend-reproducer" target="_blank">https://github.com/ahoiroman/csrf-backend-reproducer</a></p>
   <h2>Try it out for yourself</h2>
   <p>Please check your browser's cookie storage and click around for a while. You can log in multiple times, logout again etc. You'll see an XSRF-token for .domain.test. After logging in and clicking around again, you'll find another cookie named `frontend.domain.test`</p>
   <div v-if="!isLoggedIn">You may want to try it out right here? Just <NuxtLink to="/auth/signin">Log in</NuxtLink></div>
